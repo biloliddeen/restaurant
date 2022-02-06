62ecd96c61af5c64b7717f302932f9ac5a9ea1d2
Biloliddin <biloliddeen@gmail.com>
2022-02-06 20:56:36 +0500

    Create about migration

---
+++ console/migrations/m220205_142003_create_about_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%about}}`.
+ */
+class m220205_142003_create_about_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%about}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255)->notNull(),
+            'description' => $this->text(),
+            'image' => $this->string(255)->notNull(),
+            'link' => $this->string(255)->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%about}}');
+    }
+}
