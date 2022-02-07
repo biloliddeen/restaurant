0e7bd3559f04e21775d55b44e2775ce3deffe95a
Biloliddin <biloliddeen@gmail.com>
2022-02-07 22:54:08 +0500

    Create Food model

--- common/models/Food.php
+++ common/models/Food.php
@@ -10,7 +10,7 @@ use Yii;
  * @property int $id
  * @property string $name
  * @property string|null $description
- * @property int $price
+ * @property float $price
  * @property string $type
  * @property string|null $created_at
  * @property int|null $updated_by
@@ -35,8 +35,9 @@ class Food extends \yii\db\ActiveRecord
         return [
             [['name', 'price', 'type'], 'required'],
             [['description'], 'string'],
-            [['price', 'updated_by'], 'integer'],
+            [['price'], 'number'],
             [['created_at'], 'safe'],
+            [['updated_by'], 'integer'],
             [['name', 'type'], 'string', 'max' => 255],
             [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
         ];
