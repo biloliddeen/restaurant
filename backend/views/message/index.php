f032d1730df99ff6e8fa59f42fb4b56c9664d737
Biloliddin <biloliddeen@gmail.com>
2022-02-14 20:17:24 +0500

    Create Message CRUD

---
+++ backend/views/message/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\search\MessageSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Messages';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="message-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Message', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'email:email',
+            'subject',
+            'description:ntext',
+            //'created_at',
+            [
+                'class' => ActionColumn::className(),
+            ],
+        ],
+    ]); ?>
+
+
+</div>
