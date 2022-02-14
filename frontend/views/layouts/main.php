4f0c219a18b10d49de0534b6d667c2cd502aaf66
Biloliddin <biloliddeen@gmail.com>
2022-02-14 20:24:36 +0500

    Change footer link part

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -2,6 +2,7 @@
 
 /* @var $this \yii\web\View */
 /* @var $content string */
+/* @var \frontend\controllers\SiteController $contact*/
 
 use frontend\assets\AppAsset;
 use yii\bootstrap4\Breadcrumbs;
@@ -72,11 +73,10 @@ AppAsset::register($this);
             <h3>Delicious</h3>
             <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
             <div class="social-links">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <a href="<?=$contact[0]['link1']?>" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="<?=$contact[0]['link2']?>" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="<?=$contact[0]['link3']?>" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="<?=$contact[0]['link4']?>" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
             <div class="copyright">
                 &copy; Copyright <strong><span>Delicious</span></strong>. All Rights Reserved
